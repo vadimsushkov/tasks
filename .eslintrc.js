7b2f6dbff0323d9a74bbc7cc5d2ae261854e0c77
generalvadosik420@gmail.com <YOUR@EMAIL.com>
2020-10-07 11:25:34 +0300

    eslint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,23 @@
-module.exports = {
+module.exports =   {
     "env": {
-        "browser": true,
-        "es2021": true
+        "es2020": true,
+        "node": true,
     },
     "extends": [
         "airbnb-base"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": 12,
+        "ecmaVersion": 11,
         "sourceType": "module"
     },
     "plugins": [
         "@typescript-eslint"
     ],
     "rules": {
+        "max-len": [
+            "error",
+            120
+        ]
     }
 };
